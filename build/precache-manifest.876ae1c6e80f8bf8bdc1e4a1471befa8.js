b6f6d5be9d9288fad2b4d32467d9df9d9f277461
John Lee <johnmlee101@gmail.com>
2020-04-24 21:30:24 -0400

    copied over build from the private repo

---
+++ build/precache-manifest.876ae1c6e80f8bf8bdc1e4a1471befa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610fc080573cc9f33e7a71ea17a43c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b020fa3d0ad029e07ef",
+    "url": "/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "8cc0c78364b21121acab",
+    "url": "/static/css/main.1aa35e7e.chunk.css"
+  },
+  {
+    "revision": "0b020fa3d0ad029e07ef",
+    "url": "/static/js/2.1d4b0c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d4b0c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc0c78364b21121acab",
+    "url": "/static/js/main.250c3be9.chunk.js"
+  },
+  {
+    "revision": "3035c9c121da15db7ff4",
+    "url": "/static/js/runtime-main.53c48350.js"
+  }
+]);
